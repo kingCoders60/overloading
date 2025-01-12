cbb7315ad31f05567dde0b9347f747a883daef8a
kingCoders60 <spworld30@gmail.com>
2025-01-13 01:50:02 +0530

    done.

--- Inheritance/Inherit.java
+++ Inheritance/Inherit.java
@@ -1,5 +1,24 @@
-package Inheritance;
+interface A {
+    default void methodA() {
+        System.out.println("This is from interface A");
+    }
+}
+
+interface B {
+    default void methodB() {
+        System.out.println("This is from Interface B");
+    }
+}
+
+class Myclass implements A, B {
+    public void display() {
+        System.out.println("This is from myClass");
+    }
+}
 
 public class Inherit {
-
+    public static void main(String[] args) {
+        Myclass obj = new Myclass();
+        obj.display();
+    }
 }
