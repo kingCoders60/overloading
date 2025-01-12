0683422f0da4170e3748bda11300a9e9efa7d6ea
kingCoders60 <spworld30@gmail.com>
2025-01-13 01:04:34 +0530

    donw.

--- chaining/Chain.java
+++ chaining/Chain.java
@@ -6,6 +6,19 @@ class A {
 
 class B extends A {
     public B() {
-        System.out.println("B : 1")
+        // super() will be by default called!! here
+        this(4); // this is what constructor chaining..
+        System.out.println("B : 1");
+    }
+
+    public B(int k) {
+        System.out.println("B : 2");
+    }
+}
+
+class Chain {
+    public static void main(String[] args) {
+        B o1 = new B();
+
     }
 }
